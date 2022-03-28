a2449ce64ef7e41708ecf89e99021bfd75bb8e7f
Jason Boudreaux <js.bdrx@gmail.com>
2022-03-27 20:34:07 -0500

    spelling error

--- schema/index.js
+++ schema/index.js
@@ -3,7 +3,7 @@ const { buildSchema } = require('graphql');
 module.exports = buildSchema(`
 type Booking {
     _id: ID!
-    event: Event!
+    listing: Listing!
     user: User!
     createdAt: String!
     updatedAt: String!
@@ -39,8 +39,8 @@ type RootQuery {
 type RootMutation {
     createListing(listingInput: ListingInput): Listing
     createUser(userInput: UserInput): User
-    bookEvent(eventId: ID!): Booking!
-    cancelBooking(bookingId: ID!): Event!
+    bookListing(listingId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Listing!
 }
 schema {
     query: RootQuery
